54b19537c8c9df873602c84889f6100d34772de3
NaufalNafian <naufalnafian@gmail.com>
2020-05-19 06:11:30 +0700

    Initial Commit

---
+++ Owl Onboarding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Owl Onboarding
+//
+//  Created by Mohamad Naufal Nafian on 19/05/20.
+//  Copyright © 2020 Mohamad Naufal Nafian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
