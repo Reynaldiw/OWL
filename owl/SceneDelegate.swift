58c607fdddaf732b5b793d8fb6f20f4aec652c9b
Reynaldi <reynaldiwijata2306@gmail.com>
2020-05-24 19:21:29 +0700

    Delete the instance of View Controller

--- owl/SceneDelegate.swift
+++ owl/SceneDelegate.swift
@@ -18,7 +18,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let winScene = (scene as? UIWindowScene) else { return }
-        let vc = ViewController()
         let ob = OnboardingViewController()
         let ls = LoginScreen()
         let lf = LoginField()
