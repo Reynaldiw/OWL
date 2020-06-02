108685773390ad0da48a4d68d1fdc9b4e327dd6f
Reynaldi <reynaldiwijata2306@gmail.com>
2020-06-03 01:02:54 +0700

    style: Create Interface for Learn Part

--- owl/Class/View/Cell/MenuCell.swift
+++ owl/Class/View/Cell/MenuCell.swift
@@ -28,6 +28,8 @@ class MenuCell: UICollectionViewCell {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 10)
         label.textColor = Colors.bluePrimaryColor
+        label.numberOfLines = 2
+        label.textAlignment = .center
         return label
     }()
     
@@ -56,7 +58,7 @@ class MenuCell: UICollectionViewCell {
         
         imageView.anchor(top: nil, bottom: nil, leading: nil, trailing: nil, marginTop: 0, marginBottom: 0, marginLeading: 0, marginTrailing: 0, width: 35, height: 35, centerX: contentMenuView.centerXAnchor, centerY: contentMenuView.centerYAnchor, marginFromCenterX: 0, marginFromCenterY: -10)
         
-        titleLabel.anchor(top: imageView.bottomAnchor, bottom: nil, leading: nil, trailing: nil, marginTop: 5, marginBottom: 0, marginLeading: 5, marginTrailing: 5, width: 0, height: 0, centerX: contentMenuView.centerXAnchor, centerY: nil, marginFromCenterX: 0, marginFromCenterY: 0)
+        titleLabel.anchor(top: imageView.bottomAnchor, bottom: nil, leading: self.leadingAnchor, trailing: self.trailingAnchor, marginTop: 8, marginBottom: 0, marginLeading: 5, marginTrailing: -5, width: 0, height: 0, centerX: contentMenuView.centerXAnchor, centerY: nil, marginFromCenterX: 0, marginFromCenterY: 0)
     }
     
     func updateUI() {
---
+++ owl/Learn/Repository/LearnService.swift
@@ -0,0 +1,18 @@
+//
+//  LearnService.swift
+//  owl
+//
+//  Created by Reynaldi Wijaya on 02/06/20.
+//  Copyright © 2020 Reynaldi Wijaya. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+protocol LearnService {
+    func getLocalCategoryLearnModel(onSuccess: @escaping(_ categoryLearnModels: [CategoryLearnModel]) -> Void, onFailed: @escaping(_ message: String) -> Void)
+    
+    func getLocalTopicLearnModel(with request: NSFetchRequest<TopicLearnModel> , predicate: NSPredicate?, parentModel: CategoryLearnModel?, onSuccess: @escaping(_ categoryLearnModels: [TopicLearnModel]) -> Void, onFailed: @escaping(_ message: String) -> Void)
+    
+    func saveData(onSuccess: @escaping(_ message: String) -> Void, onFailed: @escaping(_ message: String) -> Void)
+}
